3c560ef5657aff295204287d2cc4094cfdebdc6e
Akshat <akshat25chauhan.2005@gmail.com>
2025-03-20 09:36:16 +0530

    initial backend setup

---
+++ backend/models/blog.js
@@ -0,0 +1,11 @@
+// models/Blog.js
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String }, // Image URL
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Blog', BlogSchema);
